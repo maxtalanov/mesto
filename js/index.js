c945f8e4c38fd4f988d72374e0dc21276099bdb5
maxtalanov <55347015+maxtalanov@users.noreply.github.com>
2020-10-17 17:18:25 +0300

    feat: Добавить элементы нового popup
    Добавить верстку стиль написать функцию на js закрытие и открытие popup

--- js/index.js
+++ js/index.js
@@ -1,25 +1,36 @@
 //Кнопки и модальное окно
 const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
+
+
+const popupCard = document.querySelector('.popup_type_card');
+const popupImg = document.querySelector('.popup_type_img');
+
 const buttonOpenPopup = document.querySelector('.profile__edit-button');
 const buttonClosePopup = document.querySelector('.form__btn-exit');
 const buttonSavePopup = document.querySelector('.form__btn-save');
-
+const buttonOpenPopupCard = document.querySelector('.profile__add-button');
 //Фрма
 const form = document.querySelector('.form');
 const formName = form.querySelector('.form__name');
 const formStatus = form.querySelector('.form__status');
 
 //Данные для формы
-let name = document.querySelector('.profile__name');
-let status = document.querySelector('.profile__status');
+const name = document.querySelector('.profile__name');
+const status = document.querySelector('.profile__status');
 
 //открытие закрытие попапа
-function popupToggle() {
+const popupToggle = (popup) => {
   popup.classList.toggle('popup_opened');
 }
-buttonOpenPopup.addEventListener('click', popupToggle);
-buttonClosePopup.addEventListener('click', popupToggle);
-buttonSavePopup.addEventListener('click', popupToggle);
+buttonOpenPopup.addEventListener('click', () => popupToggle(popupProfile));
+buttonClosePopup.addEventListener('click', () => popupToggle(popupProfile));
+buttonOpenPopupCard.addEventListener('click', () => popupToggle(popupCard));
+buttonClosePopup.addEventListener('click', () => popupToggle(popupCard));
+// buttonClosePopup.addEventListener('click', [popupProfile, popupCard, popupImg].forEach = (element) => (element.classList.remove('popup_opened'));
+//Логика делаем масив из констант. По клику на кнопку у элемента из масива должен быть удален class popup_opened;
+// buttonSavePopup.addEventListener('click', popupToggle);
+
 
 
 //вставка дданных в попап
@@ -37,6 +48,4 @@ function formSubmitHandler(evt) {
   name.textContent = formName.value;
   status.textContent = formStatus.value;
 }
-
-
 form.addEventListener('submit', formSubmitHandler);
