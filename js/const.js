247189a55529296f9e23bd7921cd2fea5a5b53f5
maxtalanov <55347015+maxtalanov@users.noreply.github.com>
2020-10-25 13:15:08 +0300

    fix: разделить директорию функций и констант в js

---
+++ js/const.js
@@ -0,0 +1,60 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Кнопки и модальное окно
+const popupProfile = document.querySelector('.popup_type_profile');
+const btnOpenPopupProfile = document.querySelector('.profile__edit-button');
+const btnExitPopupProfile = popupProfile.querySelector('.form__btn-exit');
+const btnSavePopupProfile = popupProfile.querySelector('.form__btn-save');
+
+const popupCard = document.querySelector('.popup_type_card');
+const btnOpenPopupCard = document.querySelector('.profile__add-button');
+const btnExitPopupCard = popupCard.querySelector('.form__btn-exit');
+const btnCreatePopupCard = popupCard.querySelector('.form__btn-create');
+
+const popupImg = document.querySelector('.popup_type_img');
+const btnExitPopupImg = popupImg.querySelector('.show-img__btn-exit');
+const imgEl = popupImg.querySelector('.show-img__img');
+const textEl = popupImg.querySelector('.show-img__text');
+
+//Фрма для профайла
+const form = document.querySelector('.form');
+const formName = form.querySelector('.form__name');
+const formStatus = form.querySelector('.form__status');
+
+// Форма для card
+const formCard = document.querySelector('.form-card');
+const inputName = formCard.querySelector('.form__in-name');
+const inputLink = formCard.querySelector('.form__in-link');
+
+//Данные для формы
+const name = document.querySelector('.profile__name');
+const status = document.querySelector('.profile__status');
+
+// Шаблон карточек
+const templateCard = document.querySelector('.template-card');
+const photoCard = document.querySelector('.photo-card');
