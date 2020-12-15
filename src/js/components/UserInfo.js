b2a8b1f4aa787b48298e1ecf8f34cc5051b7ebf6
maxtalanov <55347015+maxtalanov@users.noreply.github.com>
2020-12-16 01:23:15 +0300

    fix: Скорректировать название размера конструктора в классе UserInfo

--- src/js/components/UserInfo.js
+++ src/js/components/UserInfo.js
@@ -3,20 +3,20 @@ import { name, status } from  '../utils/constants.js';
 //Конец: зоны importа
 
 export class UserInfo {
-  constructor(selectorName, selectorStatus) {
-    this._selectorName = document.querySelector(selectorName);
-    this._selectorStatus = document.querySelector(selectorStatus);
+  constructor(_nameElement,  _statusElement) {
+    this._nameElement = document.querySelector(_nameElement);
+    this. _statusElement = document.querySelector( _statusElement);
   }
 
   //метод возврат obj с данными пользовотеля (публичный)
   getUserInfo() {
-    this._selectorName.value = name.textContent;
-    this._selectorStatus.value = status.textContent;
+    this._nameElement.value = name.textContent;
+    this. _statusElement.value = status.textContent;
   }
 
   //метод new данные пользовотеля + добавление их на страницу (публичный)
   setUserInfo() {
-    name.textContent = this._selectorName.value;
-    status.textContent = this._selectorStatus.value;
+    name.textContent = this._nameElement.value;
+    status.textContent = this. _statusElement.value;
   }
 }
