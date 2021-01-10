9ab54b03f13addc2d3e3baed02cf2e5e2d28befa
maxtalanov <55347015+maxtalanov@users.noreply.github.com>
2021-01-10 14:10:30 +0300

    fix: Убрать из класса UserInfo переменные импорта

--- src/js/components/UserInfo.js
+++ src/js/components/UserInfo.js
@@ -1,22 +1,21 @@
-//Старт: Зона importa
-import { name, status } from  '../utils/constants.js';
-//Конец: зоны importа
-
 export class UserInfo {
   constructor(_nameElement,  _statusElement) {
     this._nameElement = document.querySelector(_nameElement);
     this. _statusElement = document.querySelector( _statusElement);
+
+    this.nameProfile = document.querySelector('.profile__name');
+    this.statusProfile = document.querySelector('.profile__status');
   }
 
   //метод возврат obj с данными пользовотеля (публичный)
   getUserInfo() {
-    this._nameElement.value = name.textContent;
-    this. _statusElement.value = status.textContent;
+    this._nameElement.value = this.nameProfile.textContent;
+    this. _statusElement.value = this.statusProfile.textContent;
   }
 
   //метод new данные пользовотеля + добавление их на страницу (публичный)
   setUserInfo() {
-    name.textContent = this._nameElement.value;
-    status.textContent = this. _statusElement.value;
+    this.nameProfile.textContent = this._nameElement.value;
+    this.statusProfile.textContent = this. _statusElement.value;
   }
 }
